791b869aeb716dcd0dc649bd0914950d28e43f75
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-07-15 14:29:22 -0300

    Plugins

--- loja/src/test/java/br/com/alura/ProdutoTest.java
+++ loja/src/test/java/br/com/alura/ProdutoTest.java
@@ -1,14 +1,20 @@
 package br.com.alura;
 
 import static org.junit.Assert.*;
+import java.math.BigDecimal;
 
+import org.junit.Assert;
 import org.junit.Test;
+import br.com.alura.loja.Produto;
+
 
 public class ProdutoTest {
 
 	@Test
 	public void test() {
-		
+		Produto p = new Produto("teste", BigDecimal.TEN);
+		Assert.assertEquals("teste", p.getNome());
+		Assert.assertEquals(BigDecimal.TEN, p.getPreco());
 	}
 
 }
